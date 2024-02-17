947981eaee158198805262209ad25c940dac7406
Alexander <atlexeide@gmail.com>
2024-02-17 10:37:46 +0100

    Add logic to displayProjectList()

--- dom.js
+++ dom.js
@@ -8,15 +8,19 @@ export function displayProjectList() {
             const proj = document.createElement('li');
             proj.innerHTML = `
             <a href="#" class="project-card">
-            <div class="card">
+            <div class="card" id="${i}">
                 <h3>Title: ${projects[i].title}</h3>
                 <div>Desscription:</div>
                 <p>${projects[i].desc}</p>
                 <div>Priority: ${projects[i].priority}</div>
-                <div>Due by ${projects[i].dueDate}</div>
+                <div id="${i}-due">Due by ${projects[i].dueDate}</div>
             </div>
             </a>`;
             container.appendChild(proj);
+            if (!projects[i].dueDate) {
+                const due = document.getElementById(`${i}-due`);
+                due.innerHTML = '';
+            }
             projects[i].isDisplayed = true;
         }
     }
